08534f250ea7efcd8691f719f053aeb7241e4fa9
Nika <slesareva-n-r@ya.ru>
2025-04-10 21:57:57 +0300

    DB fix

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ class User(Base):
     __tablename__ = 'Users'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    vk_id: Mapped[str] = mapped_column(String(length=15), unique=True)
+    vk_id: Mapped[str] = mapped_column(String(length=20), unique=True)
 
     def __repr__(self):
         return f'User {self.id}: {self.vk_id}'
@@ -19,9 +19,9 @@ class Candidate(Base):
     __tablename__ = 'Candidates'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    vk_id: Mapped[str] = mapped_column(String(length=15), unique=True)
-    first_name: Mapped[str] = mapped_column(String(length=15), nullable=True)
-    last_name: Mapped[str] = mapped_column(String(length=15), nullable=True)
+    vk_id: Mapped[str] = mapped_column(String(length=20), unique=True)
+    first_name: Mapped[str] = mapped_column(String(length=50), nullable=True)
+    last_name: Mapped[str] = mapped_column(String(length=50), nullable=True)
     link: Mapped[str] = mapped_column(String, nullable=True)
 
     def __repr__(self):
@@ -32,7 +32,7 @@ class Photo(Base):
     __tablename__ = 'Photos'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    candidate_id: Mapped[str] = mapped_column(String(length=15), ForeignKey('Candidates.vk_id'), nullable=False)
+    candidate_id: Mapped[str] = mapped_column(String(length=20), ForeignKey('Candidates.vk_id'), nullable=False)
     link: Mapped[str] = mapped_column(String)
 
     candidate: Mapped['Candidate'] = relationship(backref='photo')
@@ -45,8 +45,8 @@ class Reaction(Base):
     __tablename__ = 'Reactions'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[str] = mapped_column(String(length=15), ForeignKey('Users.vk_id'), nullable=False)
-    candidate_id: Mapped[str] = mapped_column(String(length=15), ForeignKey('Candidates.vk_id'), nullable=False)
+    user_id: Mapped[str] = mapped_column(String(length=20), ForeignKey('Users.vk_id'), nullable=False)
+    candidate_id: Mapped[str] = mapped_column(String(length=20), ForeignKey('Candidates.vk_id'), nullable=False)
     mark: Mapped[bool] = mapped_column(nullable=True)
 
     user: Mapped['User'] = relationship(backref='reaction')
